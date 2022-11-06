8e65e734e2ef20f7c3d34d98bfa4923131178615
Denis Nikitin <dinya.nikitin@gmail.com>
2022-11-06 19:37:42 +0300

    1.0.5

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $worker = new Worker("Denis", "Nikitin", 5000, "work", 37, "JS", true, "");
 $worker2 = new Worker("Alex", "Smith", 3000, "work", 40, "PHP", false, "");
 $director = new Director("Peter", "Jons", 1000, "dir", 50, "", true, "all");
 
+echo $director->getFullName() . PHP_EOL;
+echo $director->getLeadInterface() . PHP_EOL;
 echo $worker->getFullName() . PHP_EOL;
 echo $worker->getApplicationCreatorInterface() . PHP_EOL;
 echo $worker2->getFullName() . PHP_EOL;
